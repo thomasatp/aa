6901d2da91c0a93df28f17f5fed550b8ad834c76
thomasatp <drapeau.th@gmail.com>
2024-02-01 10:08:49 +0100

    scrollY correction

--- app/ui/navigation/navContainer.tsx
+++ app/ui/navigation/navContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, } from "framer-motion";
+import { motion } from "framer-motion";
 import { useScrollDirection } from "../../hooks/useScrollDirection";
 import clsx from "clsx";
 export default function NavContainer({
@@ -7,16 +7,15 @@ export default function NavContainer({
 }: {
   children: React.ReactNode;
 }) {
-    const [isVisible, currentScroll] = useScrollDirection()
+    const [isVisible] = useScrollDirection()
 
   return (
     <motion.nav
       className={clsx(
-        "fixed top-0 left-0 z-50 grid w-full grid-cols-3 gap-6 px-6 py-8 lg:py-12 lg:px-20 transition-transform duration-300",
+        "fixed top-0 left-0 z-50 grid w-full grid-cols-3 gap-6 px-6 py-8 lg:py-12 lg:px-20 transition-transform duration-300 bg-white dark:bg-neutral-950",
         {
           "translate-y-0": isVisible,
-          "-translate-y-full": !isVisible,
-          "bg-white dark:bg-neutral-950": scrollY > 120
+          "-translate-y-full": !isVisible
         }
       )}
     >
