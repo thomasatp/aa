be17df91a39344d29598736c8d6eb647badeefbc
thomasatp <drapeau.th@gmail.com>
2024-02-14 15:50:08 +0100

    hide all data

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Skills from "./ui/homepage/skills";
 import Image from "next/image";
 import { getProjects, getHomePage } from "./lib/airtable";
 
-export const dynamic="force-dynamic"
 
 export default async function Page() {
 
@@ -13,15 +12,15 @@ export default async function Page() {
   // 1 - status : rien, Draft, Staging ou Published
   // 2 - preview: preview ou rien pour charger toutes les données
   // 3 - maxRecords : rien ou nombre de projets à afficher
-  const projects = await getProjects("Published");
+  // const projects = await getProjects("Published");
   // const homePage = await getHomePage();
 
-  console.log("coucou", projects)
+  // console.log("coucou", projects)
   return (
     <main>
       {/* <Hero title={homePage.title} description={homePage.description}  /> */}
       <div className="relative grid grid-cols-12 gap-6 gap-y-16 px-6 lg:px-20 mt-20 2xl:[&>*:nth-child(1)]:col-start-2 2xl:[&>*:nth-child(4)]:col-start-3 2xl:[&>*:nth-child(7)]:col-start-2">
-        {projects.map(({ title, tags, img, slug }, i) => (
+        {/* {projects.map(({ title, tags, img, slug }, i) => (
           <Tile
             key={i}
             title={title}
@@ -30,9 +29,9 @@ export default async function Page() {
             slug={slug}
             homepage
           />
-        ))}
+        ))} */}
       </div>
-      <Skills skills={skills} projects={projects} />
+      {/* <Skills skills={skills} projects={projects} /> */}
       <div className="relative grid grid-cols-12 gap-6 px-6 mt-20 lg:px-20">
         <p className="col-span-12 col-start-1 font-semibold text-l 2xl:col-span-2 2xl:col-start-2">
           our tools
