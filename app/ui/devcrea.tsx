f2b2928dc0b1951e0fb412631475c5acede4501f
thomasatp <drapeau.th@gmail.com>
2024-02-19 20:19:58 +0100

    blur correction

--- app/ui/devcrea.tsx
+++ app/ui/devcrea.tsx
@@ -2,44 +2,44 @@ import Link from "next/link";
 import clsx from "clsx";
 
 export default function DevCrea({ filter }: any) {
+  const dev = filter === "dev" || filter === undefined;
+  const crea = filter === "crea";
   return (
     <div className="relative grid grid-cols-12 px-6 mt-20 lg:px-20">
-      <Link
-        className="flex col-span-6 px-6 justify-end"
-        href={`about?filter=dev`}
-        scroll={false}
-      >
-        <h2
-          className={clsx(
-            "inline-block font-bold text-[10vw] blur-md transition-all duration-300",
-            {
-              "blur-none shade-text":
-                filter === "dev" || filter === undefined,
-              "hover:blur-none": filter === "crea",
-            }
-          )}
-        >
-          dev
-        </h2>
-      </Link>
-      <Link
-        className="flex col-span-6 px-6 justify-start"
-        href={`about?filter=crea`}
-        scroll={false}
-      >
-        <h2
-          className={clsx(
-            "inline-block font-bold text-[10vw] blur-md transition-all duration-300",
-            {
-              "blur-none shade-text":
-                filter === "crea",
-                "hover:blur-none": filter === "dev" || filter === undefined,
-            }
-          )}
-        >
-          crea
-        </h2>
-      </Link>
+      <div className="flex col-span-6 px-6 justify-end">
+        <Link className={clsx("cursor-pointer blur-md transition-all duration-300", {
+                "blur-none": dev,
+                "hover:blur-none": crea,
+              })} href={`about?filter=dev`} scroll={false}>
+          <h2
+            className={clsx(
+              "inline-block font-bold text-[10vw] transition-all duration-300",
+              {
+                "shade-text": dev,
+              }
+            )}
+          >
+            dev
+          </h2>
+        </Link>
+      </div>
+      <div className="flex col-span-6 px-6 justify-start">
+        <Link className={clsx("cursor-pointer blur-md transition-all duration-300", {
+                "blur-none shade-text": crea,
+                "hover:blur-none": dev,
+              })} href={`about?filter=crea`} scroll={false}>
+          <h2
+            className={clsx(
+              "inline-block font-bold text-[10vw] transition-all duration-300",
+              {
+                "shade-text": crea,
+              }
+            )}
+          >
+            crea
+          </h2>
+        </Link>
+      </div>
     </div>
   );
 }
