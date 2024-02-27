b1c1b7fc6ff37e11501f2858a23f9992f790c94d
thomasatp <drapeau.th@gmail.com>
2024-02-27 10:56:52 +0100

    correction 404

--- app/agence/page.tsx
+++ app/agence/page.tsx
@@ -9,6 +9,8 @@ import clsx from "clsx";
 import AboutFilterBar from "../ui/aboutFilterBar";
 export const dynamic = "force-dynamic";
 import Media from "../ui/project/media";
+import { brandImages } from "../lib/notion";
+import Image from "next/image";
 
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -79,6 +81,27 @@ export default async function Page({ searchParams }: { searchParams: any }) {
                 </p>
               </div>
             ))}
+        </div>
+      </div>
+      <div className="relative grid grid-cols-12 gap-6 px-6 mt-20 lg:px-20">
+        <p className="col-span-12 col-start-1 font-semibold text-l 2xl:col-span-2 2xl:col-start-2">
+          our tools
+        </p>
+        <div className="grid flex-wrap grid-cols-2 col-span-12 col-start-1 gap-2 lg:gap-6 xl:grid-cols-4 lg:grid-cols-3 2xl:col-span-7 2xl:col-start-4 opacity-40">
+          {brandImages.map((image, i) => (
+            <div
+              className="flex items-center justify-center col-span-1 bg-neutral-800 aspect-square"
+              key={i}
+            >
+              <Image
+                src={image.url}
+                alt={image.url}
+                width={image.width}
+                height={image.height}
+                className={`${image.mWidth} max-lg:h-auto`}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className="relative grid grid-cols-12 gap-6 gap-y-16 px-6 lg:px-20 mt-20 2xl:[&>*:nth-child(1)]:col-start-2 2xl:[&>*:nth-child(4)]:col-start-3 2xl:[&>*:nth-child(7)]:col-start-2 2xl:[&>*:nth-child(10)]:col-start-3 2xl:[&>*:nth-child(13)]:col-start-2">
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,6 @@ import { getError } from "./lib/getError";
 export const dynamic = "force-dynamic";
 
 export async function generateMetadata(): Promise<Metadata> {
-  
   const error = await getError();
 
   return {
@@ -16,15 +15,16 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function NotFound() {
-    const error = await getError()
+  const error = await getError();
   return (
     <main className="h-screen">
-      <Hero
-        title={error.title}
-        description={error.description}
-      />
+      <Hero title={error.title} description={error.description} />
       <div className="relative flex justify-center px-6 mt-20 lg:px-20">
-        <Link className="flex items-center h-16 px-8 text-lg transition-all duration-300 border rounded-full cursor-pointer dark:border-neutral-600 dark:hover:border-white dark:hover:bg-white dark:hover:text-neutral-950 hover:border-neutral-950 hover:bg-neutral-950 hover:text-white dark:text-white border-neutral-400 text-neutral-950" href="/">{error.button}</Link>
+        <Link href="/">
+          <button className="flex items-center h-16 px-8 text-lg transition-all duration-300 border rounded-full cursor-pointer dark:border-neutral-600 dark:hover:border-white dark:hover:bg-white dark:hover:text-neutral-950 hover:border-neutral-950 hover:bg-neutral-950 hover:text-white dark:text-white border-neutral-400 text-neutral-950">
+            {error.button}
+          </button>
+        </Link>{" "}
       </div>
     </main>
   );
